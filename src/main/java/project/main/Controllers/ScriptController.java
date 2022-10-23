96176945667ff9127aea1f0a4e2627dc4ed07292
PedroHenrique-ueg <84983523+PedroHenrique-ueg@users.noreply.github.com>
2022-10-23 15:33:49 -0300

    aaa
    aaaa

--- src/main/java/project/main/Controllers/ScriptController.java
+++ src/main/java/project/main/Controllers/ScriptController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import project.main.Models.ScriptModel;
 import project.main.Repositories.ScriptRepository;
 
+@CrossOrigin(value="localhost:4200")
 @RestController
 @RequestMapping("/script")
 public class ScriptController {
--- src/main/java/project/main/MeuGerenciadorApplication.java
+++ src/main/java/project/main/MeuGerenciadorApplication.java
@@ -2,8 +2,10 @@ package project.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableJpaRepositories( basePackages = {"project.main.Repositories"})
 public class MeuGerenciadorApplication {
 
 	public static void main(String[] args) {
